d430bc337c28c44da7965251008251e7e065b70d
kevin56908 <puustkevin@gmail.com>
2024-03-25 15:31:59 +0200

    Initial

--- src/turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_world.launch.py
+++ src/turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_world.launch.py
@@ -31,12 +31,12 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='-2.0')
-    y_pose = LaunchConfiguration('y_pose', default='-2.0')
+    y_pose = LaunchConfiguration('y_pose', default='-1.0')
 
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'worlds',
-        'maze.world'
+        'maze2.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
